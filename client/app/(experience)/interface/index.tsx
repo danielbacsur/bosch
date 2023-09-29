aaa18081e1d086c446a70ad8a40f667f2fc4712b
danielbacsur <contact@danielbacsur.com>
2023-09-29 22:23:42 +0200

    Control bar

--- client/app/(experience)/interface/index.tsx
+++ client/app/(experience)/interface/index.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import { useSocket } from "@/components/contexts/socket";
+import { useEffect } from "react";
 
 export default function Interface() {
   const socket = useSocket();
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      socket?.request.setTimestamp((prevCount: number) => prevCount + 0.01);
+    }, 10);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);  
+
   return (
-    <>
+    <div className="w-full absolute bottom-0 grid place-items-center z-10">
       <div className="flex space-x-4">
         <div>{socket?.connection.minkey}</div>
         <input
@@ -23,6 +33,6 @@ export default function Interface() {
         <div>{socket?.connection.maxkey}</div>
       </div>
       <div>{socket?.request.timestamp}</div>
-    </>
+    </div>
   );
 }
