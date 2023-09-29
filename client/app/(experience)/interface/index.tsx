2944ec983829ddbc519700a1702d362448e6d34e
danielbacsur <contact@danielbacsur.com>
2023-09-29 21:08:19 +0200

    Test interface

--- client/app/(experience)/interface/index.tsx
+++ client/app/(experience)/interface/index.tsx
@@ -1,6 +1,28 @@
+"use client";
+
+import { useSocket } from "@/components/contexts/socket";
+
 export default function Interface() {
-    return (
-      <div>Hello World!</div>
-    );
-  }
-  
+  const socket = useSocket();
+
+  return (
+    <>
+      <div className="flex space-x-4">
+        <div>{socket?.connection.minkey}</div>
+        <input
+          type="range"
+          min={socket?.connection.minkey}
+          max={socket?.connection.maxkey}
+          step={0.00000001}
+          value={socket?.request.timestamp}
+          onChange={(e) =>
+            socket?.request.setTimestamp(parseFloat(e.target.value))
+          }
+          className="pointer-events-auto"
+        />
+        <div>{socket?.connection.maxkey}</div>
+      </div>
+      <div>{socket?.request.timestamp}</div>
+    </>
+  );
+}
