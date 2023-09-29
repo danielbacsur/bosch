683f023d88735b4ba8036e9f862208a5e726505c
danielbacsur <contact@danielbacsur.com>
2023-09-30 00:04:17 +0200

    Remove wrappers

--- client/app/(experience)/page.tsx
+++ client/app/(experience)/page.tsx
@@ -4,12 +4,8 @@ import Scene from "./scene";
 export default function Home() {
   return (
     <>
-      <div className="absolute top-0 left-0 bottom-0 right-0 pointer-events-none z-10">
-        <Interface />
-      </div>
-      <div className="absolute top-0 left-0 bottom-0 right-0 pointer-events-auto z-0">
-        <Scene />
-      </div>
+      <Interface />
+      <Scene />
     </>
   );
 }
