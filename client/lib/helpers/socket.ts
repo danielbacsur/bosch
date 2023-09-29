cadee24074b887e571068cee06ec1f9c035f9c71
danielbacsur <contact@danielbacsur.com>
2023-09-29 23:44:41 +0200

    Socket helper for default response

---
+++ client/lib/helpers/socket.ts
@@ -0,0 +1,49 @@
+export const defaultResponse = {
+  timestamp: 0,
+  vehicle: {
+    yaw: 0.0,
+    speed: 0.0,
+  },
+  objects: [
+    {
+      distance: {
+        x: 0.0,
+        y: 0.0,
+      },
+      speed: {
+        x: 0.0,
+        y: 0.0,
+      },
+    },
+    {
+      distance: {
+        x: 0.0,
+        y: 0.0,
+      },
+      speed: {
+        x: 0.0,
+        y: 0.0,
+      },
+    },
+    {
+      distance: {
+        x: 0.0,
+        y: 0.0,
+      },
+      speed: {
+        x: 0.0,
+        y: 0.0,
+      },
+    },
+    {
+      distance: {
+        x: 0.0,
+        y: 0.0,
+      },
+      speed: {
+        x: 0.0,
+        y: 0.0,
+      },
+    },
+  ],
+};
