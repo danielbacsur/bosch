95790f8238230a9cf6f6d00aacdae0b6cd8514e4
danielbacsur <contact@danielbacsur.com>
2023-09-29 19:01:02 +0200

    ReconnectingWebSocket

--- client/components/contexts/socket.tsx
+++ client/components/contexts/socket.tsx
@@ -1,14 +1,29 @@
 "use client";
 
-import { createContext, useContext, ReactNode } from "react";
+import ReconnectingWebSocket from "reconnecting-websocket";
+import { createContext, useContext, ReactNode, useMemo } from "react";
 
-interface SocketContextType {}
+interface SocketContextType {
+  socket: ReconnectingWebSocket;
+}
 
 export const SocketContext = createContext<SocketContextType | null>(null);
 export const useSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }: { children: ReactNode }) => {
-  return <SocketContext.Provider value={{}}>{children}</SocketContext.Provider>;
+  const socket = useMemo(() => {
+    const webSocket = new ReconnectingWebSocket("ws://localhost:8765");
+    webSocket.onopen = () => console.log("WebSocket connection started.");
+    webSocket.onclose = () => console.log("WebSocket connection closed.");
+    webSocket.onmessage = (event) => console.log("Message recieved.");
+    return webSocket;
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
 
 export default SocketProvider;
