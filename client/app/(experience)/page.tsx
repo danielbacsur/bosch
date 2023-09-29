b632ae03e57f5ca6a031d80f32850f7f432a7dc8
danielbacsur <contact@danielbacsur.com>
2023-09-29 22:29:03 +0200

    Base layout

--- client/app/(experience)/page.tsx
+++ client/app/(experience)/page.tsx
@@ -4,8 +4,12 @@ import Scene from "./scene";
 export default function Home() {
   return (
     <>
-      <Interface />
-      <Scene />
+      <div className="absolute top-0 left-0 bottom-0 right-0 pointer-events-none z-10">
+        <Interface />
+      </div>
+      <div className="absolute top-0 left-0 bottom-0 right-0 pointer-events-auto z-0">
+        <Scene />
+      </div>
     </>
   );
 }
