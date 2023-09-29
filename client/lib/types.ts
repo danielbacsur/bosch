9c3d4cecb69f14bcffb38b875bf2d250b48da266
danielbacsur <contact@danielbacsur.com>
2023-09-29 22:29:31 +0200

    Move types to separate folder

---
+++ client/lib/types.ts
@@ -0,0 +1,45 @@
+import type ReconnectingWebSocket from "reconnecting-websocket";
+
+export interface SocketContextType {
+  socket: ReconnectingWebSocket | null;
+  connection: ConnectionType;
+  request: RequestModifierType;
+  response: ResponseType | null;
+}
+
+export interface RequestType {
+  type: "connection" | "data";
+}
+
+export interface ResponseType {
+  timestamp: number;
+  vehicle: Vehicle;
+  objects: ObjectData[];
+}
+
+export interface Vehicle {
+  yaw: number;
+  speed: number;
+}
+
+export interface ObjectData {
+  distance: Vector2D;
+  speed: Vector2D;
+}
+
+export interface Vector2D {
+  x: number;
+  y: number;
+}
+
+export interface RequestModifierType extends RequestType {
+  timestamp: number;
+  setTimestamp: any;
+}
+
+export interface ConnectionType {
+  minkey: number;
+  maxkey: number;
+  length: number;
+  deltat: number;
+}
