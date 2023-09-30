d9250501dd2edf6c3fd4883029c4e04da288f634
danielbacsur <contact@danielbacsur.com>
2023-09-30 05:14:42 +0200

    Break assistant logic

---
+++ client/components/contexts/break.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { validate } from "@/lib/helpers/three";
+import { useSocket } from "./socket";
+import {
+  createContext,
+  useContext,
+  ReactNode,
+  useEffect,
+  useMemo,
+} from "react";
+
+interface BreakContextType {
+  collision: Collision;
+}
+
+enum Collision {
+  NONE = 0,
+  CPNCO,
+  CPTA,
+  CPLA,
+  ERROR = 9,
+}
+
+export const BreakContext = createContext<BreakContextType | null>(null);
+export const useBreak = () => useContext(BreakContext);
+
+export const BreakProvider = ({ children }: { children: ReactNode }) => {
+  const socket = useSocket();
+
+  const collision = useMemo(() => {
+    if (!socket) return Collision.ERROR;
+
+    const cpnco =
+      socket.response.vehicle.speed > 0 &&
+      socket.response.vehicle.yaw === 0 &&
+      socket.response.objects.some((object) => {
+        validate(socket, object) &&
+          object.speed.x < 0 &&
+          object.speed.y === 0 &&
+          object.distance.x > 0 &&
+          object.distance.y > 0;
+      });
+
+    const cpta =
+      socket.response.vehicle.speed > 0 &&
+      socket.response.vehicle.yaw !== 0 &&
+      socket.response.objects.some((object) => validate(socket, object));
+
+    const cpla =
+      socket.response.vehicle.speed > 0 &&
+      socket.response.vehicle.yaw === 0 &&
+      socket.response.objects.some((object) => {
+        validate(socket, object) &&
+          object.speed.x === 0 &&
+          object.speed.y > 0 &&
+          object.distance.x === 0 &&
+          object.distance.y > 0;
+      });
+
+    if ([cpnco, cpta, cpla].filter(Boolean).length === 0) {
+      return Collision.NONE;
+    }
+
+    if ([cpnco, cpta, cpla].filter(Boolean).length === 1) {
+      if (cpnco) return Collision.CPNCO;
+      if (cpta) return Collision.CPTA;
+      if (cpla) return Collision.CPLA;
+    }
+
+    return Collision.ERROR;
+  }, [socket]);
+
+  useEffect(() => {
+    console.log(collision);
+  }, [collision]);
+
+  return (
+    <BreakContext.Provider value={{ collision }}>
+      {children}
+    </BreakContext.Provider>
+  );
+};
