ff4677a8ca53dd5dacc4464c816005dcf3b50a94
danielbacsur <contact@danielbacsur.com>
2023-09-30 03:51:39 +0200

    Dummy getBreakDistance

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getBreakDistance(speed: number) {
+  return 10
+}
