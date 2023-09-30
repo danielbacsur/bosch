e13da1b1e1f89dbabaae66b02f976c95b1c24b10
danielbacsur <contact@danielbacsur.com>
2023-09-30 12:30:02 +0200

    Break distance

--- client/lib/helpers/three.ts
+++ client/lib/helpers/three.ts
@@ -20,8 +20,14 @@ export const validate = (socket: SocketContextType, object: ObjectData) => {
   return inDanger && isValid;
 };
 
-export function getBreakDistance(speed?: number) {
-  return 10;
+
+export function getBreakDistance(vEgo: number, aMax: number = -9, jerk: number = -30): number {
+  const t = aMax / Math.abs(jerk);
+  const s1 = vEgo * t + 0.5 * 0.5 * aMax * t * t;
+  const v1 = vEgo + aMax * t;
+  const s2 = -v1 * v1 / (2 * aMax);
+
+  return s1 + s2 + 2.4;
 }
 
 export function convert (vec: Vector2D) {
