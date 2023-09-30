5c0e6ae0aa0293765d7d11819b46364971f9c505
danielbacsur <contact@danielbacsur.com>
2023-09-30 02:43:04 +0200

    Update index.tsx

--- client/app/(experience)/interface/index.tsx
+++ client/app/(experience)/interface/index.tsx
@@ -1,28 +1,33 @@
 "use client";
 
 import { useSocket } from "@/components/contexts/socket";
+import { calculateBreakDistance } from "@/lib/utils";
 
 export default function Interface() {
   const socket = useSocket();
 
   return (
-    <div className="absolute bottom-[8vh] left-1/2 -translate-x-1/2 z-10">
-      <div className="flex space-x-4">
-        <div>{socket?.connection.minkey}</div>
-        <input
-          type="range"
-          min={socket?.connection.minkey}
-          max={socket?.connection.maxkey}
-          step={0.00000001}
-          value={socket?.request.timestamp}
-          onChange={(e) =>
-            socket?.request.setTimestamp(parseFloat(e.target.value))
-          }
-          className="pointer-events-auto"
-        />
-        <div>{socket?.connection.maxkey}</div>
+    socket && (
+      <div className="absolute bottom-[8vh] left-1/2 -translate-x-1/2 z-10">
+        <div className="flex space-x-4">
+          <div>{socket.connection.minkey}</div>
+          <input
+            type="range"
+            min={socket.connection.minkey}
+            max={socket.connection.maxkey}
+            step={0.00000001}
+            value={socket.request.timestamp}
+            onChange={(e) =>
+              socket.request.setTimestamp(parseFloat(e.target.value))
+            }
+            className="pointer-events-auto"
+          />
+          <div>{socket.connection.maxkey}</div>
+        </div>
+        <div>{socket.request.timestamp}</div>
+        <div>{socket.response.vehicle.speed * 3.6}</div>
+        <div>{calculateBreakDistance(socket.response.vehicle.speed)}</div>
       </div>
-      <div>{socket?.request.timestamp}</div>
-    </div>
+    )
   );
 }
