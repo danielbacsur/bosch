ea65777d1d3bc38605eca8863ef1c103cfd56250
danielbacsur <contact@danielbacsur.com>
2023-09-30 05:14:01 +0200

    Validate object

--- client/lib/helpers/three.ts
+++ client/lib/helpers/three.ts
@@ -1,4 +1,5 @@
 import { Vector3 } from "three";
+import { ObjectData, SocketContextType } from "../types";
 
 export function distanceVector(v1: Vector3, v2: Vector3) {
   var dx = v1.x - v2.x;
@@ -7,3 +8,14 @@ export function distanceVector(v1: Vector3, v2: Vector3) {
 
   return Math.sqrt(dx * dx + dy * dy + dz * dz);
 }
+
+export const validate = (socket: SocketContextType, object: ObjectData) => {
+  const inDanger =
+    distanceVector(
+      new Vector3(0, 0, 0),
+      new Vector3(object.distance.x, 0, object.distance.y)
+    ) < getBreakDistance(socket.response.vehicle.speed);
+  const isValid = object.distance.x !== 0 && object.distance.y !== 0;
+
+  return inDanger && isValid;
+};
